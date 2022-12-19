10f915157559547be291eaffb0b1c752ee24e8e0
Husanboy <olimboyevhusanboy@gmail.com>
2022-12-19 15:05:21 +0500

    yes

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int s = sideLength / 2, i;
+        for (i = 0; i < sideLength; i++) {
+            String w = "";
+            for (int j = 0; j < sideLength; j++) {
+                if (i != s) {
+                    w+= j != s ? " " : "8";
+                } else {
+                    w+="8";
+                }
+            }
+            System.out.println(w);
+        }
 
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,58 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int m=0;
+        if(height%2==0){
+            m=height/2;
+        }else {
+            m=height/2+1;
+        }
 
+        for(int i=0;i<m;i++){
+            int l=i;
+            int r=height-1-i;
+            for(int j=0;j<height;j++){
+                if(l<=j&&j<=r){
+                    System.out.print('8');
+                }else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("");
+        }
+
+        int st=0;
+        if(height%2==1){
+            st=1;
+        }
+        for(int i=st;i<m;i++){
+            int l=(m-1-i);
+            int r=height-1-(m-1-i);
+            for(int j=0;j<height;j++){
+                if(l<=j&&j<=r){
+                    System.out.print('8');
+                }else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("");
+        }
+        //       if (height % 2 == 0){
+//
+//       }
+//       else {
+//           int s = height / 2, i;
+//           for (i = 0; i < height; i++) {
+//               String w = "";
+//               for (int j = 0; j < height; j++) {
+//                   if (i == s) {
+//                       w+= j != s ? " " : "8";
+//                   } else {
+//                       w+="8";
+//                   }
+//               }
+//               System.out.println(w);
+//           }
+//       }
     }
 }
